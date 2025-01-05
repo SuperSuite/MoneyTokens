c8642576d077e7c158228fcabfcfd78d24cfca79
MLGPenguin <baww123@gmail.com>
2025-01-05 18:08:21 +1000

    init from template

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "1.9.0"
+    id("com.gradleup.shadow") version "8.3.5"
+}
+
+group = "com.github.mlgpenguin"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://www.jitpack.io")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
+    implementation("com.github.SuperGlueLib:SuperGlue:1.1.2")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks {
+    processResources {
+        val props = LinkedHashMap(mapOf("version" to version, "project_name" to rootProject.name))
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand (props)
+        }
+    }
+    shadowJar {
+        minimize()
+        dependencies {
+            exclude(dependency("org.jetbrains.kotlin::"))
+        }
+    }
+
+}
---
+++ src/main/java/com/github/mlgpenguin/plugintemplate/PluginTemplate.kt
@@ -0,0 +1,11 @@
+package com.github.mlgpenguin.plugintemplate
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PluginTemplate: JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+}
